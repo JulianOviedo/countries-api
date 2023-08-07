3f7aa8eb4217c0fad15ad9405454f22646dc1346
JulianOviedo <julianovie234@gmail.com>
2023-08-07 11:41:30 -0300

    add eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    './node_modules/ts-standard/eslintrc.json',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
@@ -14,5 +15,9 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'react-refresh/only-export-components': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/promise-function-async': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
   },
 }
