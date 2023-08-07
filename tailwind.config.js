18e0fb3648d29a0d98998c43f6144f1334c7f1e0
JulianOviedo <julianovie234@gmail.com>
2023-08-07 16:43:43 -0300

    mobile design

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,17 +7,20 @@ export default {
   theme: {
     colors: {
      'd-blue': 'hsl(209, 23%, 22%)',
-     'd-blue-bg': 'hsl(209, 23%, 22%)',
-     'l-blue-text': 'hsl(209, 23%, 22%)',
-     'l-gray-input': 'hsl(209, 23%, 22%)',
-     'd-gray-input': 'hsl(209, 23%, 22%)',
-     'l-gray-bg': 'hsl(209, 23%, 22%)',
-     'white': 'hsl(209, 23%, 22%)',
-
+     'd-blue-bg': 'hsl(207, 26%, 17%)',
+     'l-blue-text': 'hsl(200, 15%, 8%)',
+     'l-gray-input': 'hsl(0, 0%, 52%)',
+     'l-gray-bg': 'hsl(0, 0%, 98%)',
+     'white': 'hsl(0, 0%, 100%)',
     },
     extend: {},
+    fontFamily: {
+      primary: "'Nunito Sans', sans-serif"
+    }
+
   },
   plugins: [],
+  darkMode: 'class',
 }
 
 // - Dark Blue (Dark Mode Elements): hsl(209, 23%, 22%)
@@ -26,3 +29,7 @@ export default {
 // - Dark Gray (Light Mode Input): hsl(0, 0%, 52%)
 // - Very Light Gray (Light Mode Background): hsl(0, 0%, 98%)
 // - White (Dark Mode Text & Light Mode Elements): hsl(0, 0%, 100%)
+
+
+// - Family: [Nunito Sans](https://fonts.google.com/specimen/Nunito+Sans)
+// - Weights: 300, 600, 800
